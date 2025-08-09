250e22d64460425243aaaeda6f57be53b8cb105e
Vimal <vimal2409it@gmail.com>
2025-08-09 16:12:47 +0530

    vite updated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -16,7 +15,11 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
-    }
-  }
-})
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true, // good to avoid CORS errors
+        secure: false,
+      },
+    },
+  },
+});
